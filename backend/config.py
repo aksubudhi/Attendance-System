6391e6fd090c98ca986b21e0d9cef2d398b51967
aksubudhi <subudhi138@gmail.com>
2025-12-06 12:03:37 +0530

    FIX-Add frontend in config

--- backend/config.py
+++ backend/config.py
@@ -10,6 +10,7 @@ class Settings:
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
     DB_NAME = os.getenv("DB_NAME", "face_attendance")
+    FRONTEND_HOST = os.getenv("FRONTEND_HOST", "http://localhost:5173")
 
     # Camera
     CAMERA_URL_FILE = "camera_urls.json"
